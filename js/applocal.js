f17275352318a936d69121a49a13e455ff56c1f2
yuyunyuniati <151653069+yuyunyuniati@users.noreply.github.com>
2024-07-13 16:47:21 +0700

    Create applocal.js

---
+++ js/applocal.js
@@ -0,0 +1,15 @@
+import { util } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/util.js';
+import { like } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/like.js';
+import { theme } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/theme.js';
+import { audio } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/audio.js';
+import { comment } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/comment.js';
+import { progress } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/progress.js';
+import { pagination } from 'https://raw.githubusercontent.com/yuyunyuniati/Wedding-Invitation-Dimas-Yuyun/main/js/pagination.js';
+
+window.util = util;
+window.like = like;
+window.theme = theme;
+window.audio = audio;
+window.comment = comment;
+window.progress = progress;
+window.pagination = pagination;
